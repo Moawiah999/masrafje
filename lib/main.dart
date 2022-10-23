d34719b5ff86dc3b2cfb3bf8a864ad0a0045f482
Moawiah999 <muawiyah454@gmail.com>
2022-10-24 01:09:10 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mymasrafja/screen/contactus.dart';
+import 'package:mymasrafja/screen/membership.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Contact_us();
+    return Membership();
   }
 }
--- lib/screen/membership.dart
+++ lib/screen/membership.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Membership extends StatefulWidget {
   const Membership({super.key});
 
@@ -9,6 +10,140 @@ class Membership extends StatefulWidget {
 class _MembershipState extends State<Membership> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "Membership",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue.shade900),
+              child: Center(
+                //     color: Colors.blue.shade900,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 350,
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Individual",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Membership",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 130),
+                        child: Divider(
+                          thickness: 1,
+                        ),
+                      ),
+                      Text("\$ 5",
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            //   ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: 350,
+              height: 250,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: const Offset(
+                    5.0,
+                    5.0,
+                  ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(15)),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    alignment: Alignment.topLeft,
+                    child: Text("Features"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 22),
+                    alignment: Alignment.topLeft,
+                    child: Text("1.Control Saving "),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(22),
+                    alignment: Alignment.topLeft,
+                    child: Text("2.Control oblegations"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: Text(
+                        "Lorem ipsum dolor sit amet,consectetuer"
+                        "adipiscing elit,sed diam nonummy nibh"
+                        "euismod tincidunt ut laoreet dolore magna",
+                        style: TextStyle(height: 1.5),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: 250,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(15)),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue.shade900,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text("Continue for payment"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
