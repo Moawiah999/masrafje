24e9f9fe5ca2790521b6a046316358aecf9983aa
Moawiah999 <muawiyah454@gmail.com>
2022-10-23 23:32:57 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mymasrafja/screen/contactus.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Contact_us();
+  }
+}
---
+++ lib/screen/membership.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Membership extends StatefulWidget {
+  const Membership({super.key});
+
+  @override
+  State<Membership> createState() => _MembershipState();
+}
+
+class _MembershipState extends State<Membership> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
